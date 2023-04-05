721cf70204f023dfb9a6cade77f8de6e976e984d
Buzanov Kostya <buzanov.kostya@gmail.com>
2023-04-05 14:34:43 +0300

    Update ModalMap.jsx

--- src/components/Modal/ModalMap/ModalMap.jsx
+++ src/components/Modal/ModalMap/ModalMap.jsx
@@ -1,9 +1,6 @@
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import {
-  toggleShowModalAddPond,
-  toggleShowModalMap,
-} from 'redux/modal/modalSlice';
+import { toggleShowModalMap } from 'redux/modal/modalSlice';
 
 import ModalUniversal from '../ModalUniversal';
 import NewMap from 'components/Map/Map';
