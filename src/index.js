c81f5032e655a28905fd258986a166be0c5ea60c
Buzanov Kostya <107797404+JIyk777@users.noreply.github.com>
2024-01-02 16:13:05 +0200

    install react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from 'redux/store';
@@ -15,9 +16,11 @@ root.render(
   <ThemeProvider theme={theme}>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </PersistGate>
     </Provider>
-  </ThemeProvider>
+  </ThemeProvider>,
   // </React.StrictMode>
 );
