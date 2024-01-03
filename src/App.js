8aa37eba6922fbf9b2d9267d47e943e392ad09eb
Buzanov Kostya <107797404+JIyk777@users.noreply.github.com>
2024-01-03 10:57:57 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import HomePage from 'pages/HomePage';
+// import RegisterPage from './pages/register/RegisterPage';
+
+import { lazy, Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+const Login = lazy(() => import('pages/login/LoginPage'));
+const HomePage = lazy (() => import('pages/homePage/HomePage'));
+const RegisterPage = lazy(() => import('pages/register/RegisterPage'));
+
 
 function App() {
-  return <HomePage />;
+
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<RegisterPage />} />
+        {/* Інші маршрути */}
+      </Routes>
+    </Suspense>
+  );
 }
 
 export default App;
