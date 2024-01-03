9ac536ad384becd664676e0289420af4460c0854
Buzanov Kostya <107797404+JIyk777@users.noreply.github.com>
2024-01-03 16:59:21 +0200

    webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  entry: `${__dirname}/src/index.js`,
+  entry: `${__dirname}/src/index.tsx`,
   output: {
     path: `${__dirname}/build`,
     publicPath: '/build/',
