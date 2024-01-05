0f48b67d2b481dd837101574b38747e47631b2ea
Buzanov Kostya <107797404+JIyk777@users.noreply.github.com>
2024-01-05 10:08:43 +0200

    delete onAuthStateChanged

--- src/components/AuthBar/AuthBar.jsx
+++ src/components/AuthBar/AuthBar.jsx
@@ -1,27 +1,13 @@
 import { AuthBarContainer, AuthBarText, LogOut } from './AuthBar.styled';
 import { MdExitToApp } from "react-icons/md";
-import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { signOut } from 'firebase/auth';
 import { auth } from '../../firebaseConfig';
 
 const AuthBar = () => {
   const handelLogOut = () => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        console.log('online')
-      } else {
-        console.log('offline');
-      }})
     signOut(auth).then(() => {
-
     }).catch((error) => {
-      // An error happened.
     });
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        console.log('online')
-      } else {
-        console.log('offline');
-      }})
   }
 
   return (
